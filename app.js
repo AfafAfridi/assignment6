427d6ff5c1fd5439fd6f340b97eb74bccd4ff433
Afaf Afridi <afaf.afridi83@gmail.com>
2024-03-29 16:50:51 +0500

    files

---
+++ app.js
@@ -0,0 +1,52 @@
+//Addition:
+
+// let number1 = prompt("Enter the first number:");
+// let number2 = prompt("Enter the second number:");
+// number1 = parseFloat(number1); // change the string to integer
+// number2 = parseFloat(number2);
+
+// let result = number1 + number2;
+
+// // Displaying the result
+// console.log("Result of addition:", result);
+// alert("Result of addition: " + result);
+
+// Subtraction:
+// let number1 = prompt("Enter the first number:");
+// let number2 = prompt("Enter the second number:");
+
+// number1 = parseFloat(number1);
+// number2 = parseFloat(number2);
+
+
+// let result = number1 - number2;
+
+// console.log("Result of subtraction:", result);
+
+// alert("Result of subtraction: " + result);
+
+// Multiplication
+
+// let number1 = prompt("Enter the first number:");
+// let number2 = prompt("Enter the second number:");
+// number1 = parseFloat(number1);
+// number2 = parseFloat(number2);
+// let result = number1 * number2;
+
+
+// console.log("Result of multiplication:", result);
+
+// alert("Result of multiplication: " + result);
+
+// Division
+
+let number1 = prompt("Enter the first number:");
+let number2 = prompt("Enter the second number:");
+number1 = parseFloat(number1);
+number2 = parseFloat(number2);
+
+let result = number1 / number2;
+
+
+console.log("Result of division:", result);
+alert("Result of division: " + result);
